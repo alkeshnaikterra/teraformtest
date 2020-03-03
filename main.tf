4e22c05b06320b52f9fa95920e9ba7b2cb3ef7bf
Naik <alkesh.naik@atos.net>
2020-03-03 19:19:56 +0530

    deleting the network

--- main.tf
+++ main.tf
@@ -8,20 +8,3 @@ resource "azurerm_resource_group" "rg" {
     name     = "${var.resource_group_name}"
     location = "${var.location}"
 }
-
-resource "azurerm_virtual_network" "hub-vnet" {
-    provider = "azurerm.core"
-  name                = "${local.prefix-hub}-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-
-}
-
-resource "azurerm_subnet" "hub-gateway-subnet" {
-    provider = "azurerm.core"
-  name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.hub-vnet.name
-  address_prefix       = "10.0.255.224/27"
-}
