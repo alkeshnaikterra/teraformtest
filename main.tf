caa861f6fca9f5058f96465eea4dc19cabb4043b
Naik <alkesh.naik@atos.net>
2020-03-06 12:34:30 +0530

    removed the

--- main.tf
+++ main.tf
@@ -8,3 +8,10 @@ resource "azurerm_resource_group" "rg" {
     name     = "${var.resource_group_name}"
     location = "${var.location}"
 }
+resource "null_resource" "script1" {
+  provisioner "local-exec" {
+    command = "Write-Host \"Hello ${var.tenant_id}\""
+
+    interpreter = ["PowerShell", "-Command"]
+  }
+}
