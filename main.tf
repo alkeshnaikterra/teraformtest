bfea32b7957c624b405832379d533e582338e17d
Naik <alkesh.naik@atos.net>
2020-03-06 12:35:40 +0530

    removed the rg

--- main.tf
+++ main.tf
@@ -2,16 +2,3 @@
 locals {
   prefix-hub         = "hub"
 }
-
-resource "azurerm_resource_group" "rg" {
-    provider = "azurerm.core"
-    name     = "${var.resource_group_name}"
-    location = "${var.location}"
-}
-resource "null_resource" "script1" {
-  provisioner "local-exec" {
-    command = "Write-Host \"Hello ${var.tenant_id}\""
-
-    interpreter = ["PowerShell", "-Command"]
-  }
-}
