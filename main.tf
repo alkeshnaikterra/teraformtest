04001d8f8ee732c772f13b5ca24e3e86a8600f0b
Naik <alkesh.naik@atos.net>
2020-03-06 12:39:34 +0530

    added a rg

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@
 locals {
   prefix-hub         = "hub"
 }
+
+resource "azurerm_resource_group" "rg" {
+    provider = "azurerm.core"
+    name     = "${var.resource_group_name}"
+    location = "${var.location}"
+}
