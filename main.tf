137d4c1d424fbcba15126d159c71d744e3de9ef3
Naik <alkesh.naik@atos.net>
2020-03-06 16:13:09 +0530

    adding rg demo

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ locals {
   prefix-hub         = "hub"
 }
 
+resource "azurerm_resource_group" "rg" {
+    provider = "azurerm.core"
+    name     = "${var.resource_group_name}"
+    location = "${var.location}"
+}
